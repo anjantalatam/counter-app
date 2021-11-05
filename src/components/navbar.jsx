0a7a757ad9dafd75a286b46624d22882feee8b76
Anjan Talatam <tspa2117@gmail.com>
2021-11-05 19:05:59 +0530

    Update Navbar Total Counter Functionality

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,13 +6,17 @@ import React from "react";
 // }
 
 const NavBar = (props) => {
+  // console.log(props.counters);
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="www.youtube.com">
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
           {/* there is no this.props in here so we use props as an argument  */}
-          {props.counters.filter((m) => m.value !== 0).length}
+          {props.counters.reduce(
+            (totalCounters, counter) => totalCounters + counter.value,
+            0
+          )}
         </span>
       </a>
     </nav>
